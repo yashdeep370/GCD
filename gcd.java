107529b9fc08c19cac575df03def1c81553a60d1
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-01-14 22:22:20 +0530

    Add files via upload

---
+++ gcd.java
@@ -0,0 +1,21 @@
+public class gcd {
+    
+    static int gcd(int m,int n)
+    {
+        while(m!= n)
+        {
+            if(m>n)
+            {
+                m=m-n;
+            }
+            else
+            {
+                n=n-m;
+            }
+        }
+        return m;
+    }
+    public static void main(String[] args) {
+        System.out.println(gcd(35,56));    
+    }
+}
